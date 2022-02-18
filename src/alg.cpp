887bdf351317046c0b120fde114e85b25eb8ca5f
Дреняев Максим <drenyaev.max@gmail.com>
2022-02-18 10:53:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,7 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-#include <iostream>
+#include <cmath>
 
 bool checkPrime(uint64_t value) {
   for (int i = 2; i <= sqrt(value); i++) {
